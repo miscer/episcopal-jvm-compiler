61c339dc356acbbeda4d7d707008cd321bb92782
Michal Miškerník <michal@miskernik.sk>
2017-11-01 19:00:05 +0000

    Comments for syntax

--- Syntax.hs
+++ Syntax.hs
@@ -9,37 +9,70 @@ module Syntax (
   Operator (..)
 ) where
 
+-- | Identifier
 type Id = String
 
-data Program = Program Id Expression [Query]
-               deriving Show
+-- | Episcopal Program
+data Program
+  -- | Program with a name, program expression and a number of queries
+  = Program Id Expression [Query]
+    deriving Show
 
-data Query = Query Id [Id] [Expression]
+-- | Program query
+data Query
+  -- | Query with a name, parameter names and the query expression
+  = Query Id [Id] [Expression]
              deriving Show
 
-data Expression = ExpConst Constant
-                | ExpLet [Definition] Expression
-                | ExpObserve Expression Expression
-                | ExpSample Expression
-                | ExpDist Distribution
-                | ExpCall Id [Expression]
-                | ExpOp Operator Expression Expression
-                  deriving Show
+-- | Expression
+data Expression
+  -- | Constant
+  = ExpConst Constant
+  -- | Local definitions scoped to the expression
+  | ExpLet [Definition] Expression
+  -- | Observation
+  -- | The first expression should be a sample
+  | ExpObserve Expression Expression
+  -- | Sample of a distribution
+  | ExpSample Expression
+  -- | Distribution
+  | ExpDist Distribution
+  -- | Function call for the specified function with the specified arguments
+  -- | This is also used to call queries and access local variables
+  | ExpCall Id [Expression]
+  -- | Binary operator with the specified expressions on the left and right hand side
+  | ExpOp Operator Expression Expression
+    deriving Show
 
-data Constant = ConstInt Int
-              | ConstFloat Float
-              | ConstBool Bool
-                deriving Show
+-- | Constant
+data Constant
+  -- | Integer constant
+  = ConstInt Int
+  -- | Floating point Constant
+  -- | Also used to store percentages (i.e. probability)
+  | ConstFloat Float
+  -- | Boolean constant
+  | ConstBool Bool
+    deriving Show
 
-data Definition = Definition Id [Id] [Expression]
-                  deriving Show
+-- | Definition
+data Definition
+  -- | Local variable/function definition with the specified name, list of parameters and the value expression
+  = Definition Id [Id] [Expression]
+    deriving Show
 
-data Distribution = Bernoulli Expression
-                  | Beta Expression Expression
-                  | Normal Expression Expression
-                  | Flip Expression
-                  | Distribution Id [Expression]
-                    deriving Show
+-- | Distribution definition
+data Distribution
+  -- | Bernoulli distribution with the specified probability (p)
+  = Bernoulli Expression
+  -- | Beta distribution with pseudocounts a and b
+  | Beta Expression Expression
+  -- | Normal distribution with mean (m) and standard deviation (sd)
+  | Normal Expression Expression
+  -- | Flip distribution with the specified probability (p)
+  | Flip Expression
+    deriving Show
 
+-- | Binary operators
 data Operator = OpPlus | OpMinus | OpTimes | OpOver | OpOr | OpAnd | OpLessThan | OpGreaterThan | OpEqual
                 deriving Show
